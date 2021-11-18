ff6fc77e8a27ba2c4503e115e39965a627013f42
Adoankim <adoankim@gmail.com>
2021-11-18 20:56:54 +0000

    add player controller and movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10.0f;
+
+    [SerializeField]
+    private float boost = 2.0f;
+
+    private Rigidbody2D _rigidbody;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        bool withBoost = Input.GetKey(KeyCode.LeftShift);
+
+        if (h > 0)
+        {
+            _rigidbody.velocity = new Vector2(speed * (withBoost ? boost : 1), _rigidbody.velocity.y);
+        }
+        else if (h < 0)
+        {
+            _rigidbody.velocity = new Vector2(-speed * (withBoost ? boost : 1), _rigidbody.velocity.y);
+        }
+        else
+        {
+            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        }
+    }
+}
