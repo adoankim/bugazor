5b21cb8c119de28dc7c0d224dacc27d0770e1148
Adoankim <adoankim@gmail.com>
2021-11-29 19:13:59 +0000

    finalize console controller prefab and add rotate around movement script

---
+++ Assets/Scripts/Enemies/RotateAround.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 100f;
+
+    [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    private Vector3? axis;
+
+    [SerializeField]
+    private bool randomStart = false;
+
+    [SerializeField]
+    private float startRange = 4.0f;
+
+    private void Start()
+    {
+        if(axis == null)
+        {
+            axis = Vector3.forward;
+        }
+
+        if (randomStart)
+        {
+            target.transform.Translate(
+                new Vector3(
+                    Random.Range(-startRange, startRange),
+                    Random.Range(-startRange, startRange), 
+                    Random.Range(-startRange, startRange)
+                )
+            );
+        }
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        target.transform.RotateAround(this.transform.position, axis.Value, Time.deltaTime * speed);
+    }
+}
